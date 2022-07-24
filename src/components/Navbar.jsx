e4fef73e398998ea806d22442dfbbaeb14baacb5
hasinirajasinghe <hasinichathu@gmail.com>
2022-07-24 18:05:54 -0400

    homepage setup finished w/o CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,13 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-        {/* TODO: Add logo to the navbar */}
+        {/* TODO: HR: Add logo to the navbar */}
         <ul>
             <Link to='/auth/login'>Log In</Link>
             <Link to='/auth/signup'>Sign Up</Link>
+            {/* HR: For logged in user only */}
+            {/* <Link to='/trails/new'>Create New Trail</Link> */}
+            {/* <Link to='/trails/???'>Log Out</Link> */}
         </ul>
     </div>
   )
--- src/components/Trail.jsx
+++ src/components/Trail.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const Trail = () => {
+const Trail = ({trail}) => {
   return (
-    <div>Trail</div>
+    <div>
+      <div>
+        <img src={trail.image} alt={trail.name} />
+      </div>
+      <div>
+        <h3>{trail.name}</h3>
+        <Link to={`/trails/${trail._id}`}>More Details</Link>
+      </div>
+    </div>
   )
 }
 
