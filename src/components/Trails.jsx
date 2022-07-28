2c395a2957ac50c305b04152a9e53186f379c4c4
hasinirajasinghe <hasinichathu@gmail.com>
2022-07-27 22:26:29 -0400

    homepage styling partially done

--- src/components/Trails.jsx
+++ src/components/Trails.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Trail from '../components/Trail'
+import styled from 'styled-components'
+
+const TrailsContainer = styled.div`
+    min-width: 100vw;
+    display:  flex;
+    flex-wrap: wrap;
+    gap: 5px;
+`
 
 const Trails = ({trails}) => {
   return (
-    <div>
+    <TrailsContainer>
       {trails.map((trail) => {
         return (
           <Trail trail={trail} key={trail._id}/>
         )
       })}
-    </div>
+    </TrailsContainer>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,22 @@
 import Trails from "../components/Trails";
+import styled from 'styled-components'
+
+const HomepageBody = styled.div`
+    h1 {
+        font-size: 50px;
+        text-align: center;
+    }
+`
 
 const Home = ({trails, updateTrailState}) => {
     return (
-        <div>
-            {/* HR: Potential App Name? */}
+        <HomepageBody>
+            <img src="../../public/assets/homepage-background.png" alt="background" />
             <h1>Take a Hike</h1>
             <div>
                 <Trails trails={trails} updateTrailState={updateTrailState}/>
             </div>
-        </div>
+        </HomepageBody>
     );
 };
 
