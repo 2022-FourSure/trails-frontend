729aafdd2f45323f72e9fd4f3764321190498e20
carolinexc <caroline@humanperspectives.com>
2022-08-04 22:06:18 -0400

    home styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Trails from "../components/Trails";
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
 // const HomepageBody = styled.div`
 //     h1 {
@@ -9,22 +9,33 @@ import styled from 'styled-components'
 //     }
 // `
 
-const BackgroundImage = styled.div`
-    opacity: 0.35;
-    img {
-        width: 100%;
-    }
-    margin-bottom: -100px;
-`
+// const BackgroundImage = styled.div`
+//     opacity: 0.35;
+//     img {
+//         width: 100%;
+//     }
+//     margin-bottom: -100px;
+// `
 const Home = ({trails, updateTrailState}) => {
     return (
         // <HomepageBody>
-        <div>
-            {/* <BackgroundImage>
-                <div>
-                    <img src="/assets/background_image.jpg" alt="background" />
-                </div>
-            </BackgroundImage> */}
+        <div className="home">
+
+            {/* <BackgroundImage> */}
+            <div className="box">
+                <img className="bg-img" src="/assets/background_image.jpg" alt="background" />   
+            </div>
+            {/* </BackgroundImage> */}
+            <div className="box-overlay">
+                <figure className="text-center">
+                        <blockquote className="blockquote h5">
+                            <p>There are no shortcuts to any place worth going.</p>
+                        </blockquote>
+                        <figcaption className="blockquote-footer">
+                            Beverly Sills <cite title="Source Title">Source Title</cite>
+                        </figcaption>
+                    </figure>          
+            </div>
             <div>
                 <Trails trails={trails} updateTrailState={updateTrailState}/>
             </div>            
